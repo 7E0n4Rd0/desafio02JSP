2089accbab83e6c7a0965cfcdbac05b28d602e76
leonArch <leomeireles66@gmail.com>
2024-12-21 07:45:50 -0300

    Project created

---
+++ src/main/java/com/leonardo/desafio02JSP/Desafio02JspApplication.java
@@ -0,0 +1,13 @@
+package com.leonardo.desafio02JSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafio02JspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafio02JspApplication.class, args);
+	}
+
+}
