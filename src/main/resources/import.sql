6363561bee01f8d6500ff2475e88a69859f0bf98
leonArch <leomeireles66@gmail.com>
2024-12-21 11:16:08 -0300

    import.sql fixed and finished

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -9,9 +9,9 @@ INSERT INTO tb_categoria(id, descricao) VALUES (2, 'Oficina');
 INSERT INTO tb_atividade(id, nome, descricao, preco, categoria_id) VALUES (1, 'Curso de HTML', 'Aprenda HTML de forma prática', '80.00', 1);
 INSERT INTO tb_atividade(id, nome, descricao, preco, categoria_id) VALUES (2, 'Oficina de Github', 'Controle Versões do seus projetos', '50.00', 2);
 
-INSERT INTO tb_bloco(id, inicio, fim, atividade_id) VALUES (1, '2017-09-25T11:00:00Z', '2017-09-25T14:00:00Z', 1);
-INSERT INTO tb_bloco(id, inicio, fim, atividade_id) VALUES (2, '2017-09-25T17:00:00Z', '2017-09-25T21:00:00Z', 2);
-INSERT INTO tb_bloco(id, inicio, fim, atividade_id) VALUES (3, '2017-09-26T11:00:00Z', '2017-09-26T14:00:00Z', 2);
+INSERT INTO tb_bloco(id, inicio, fim, atividade_id) VALUES (1, TIMESTAMP WITHOUT TIME ZONE '2017-09-25T08:00:00Z', TIMESTAMP WITHOUT TIME ZONE '2017-09-25T11:00:00Z', 1);
+INSERT INTO tb_bloco(id, inicio, fim, atividade_id) VALUES (2, TIMESTAMP WITHOUT TIME ZONE '2017-09-25T14:00:00Z', TIMESTAMP WITHOUT TIME ZONE '2017-09-25T18:00:00Z', 2);
+INSERT INTO tb_bloco(id, inicio, fim, atividade_id) VALUES (3, TIMESTAMP WITHOUT TIME ZONE '2017-09-26T08:00:00Z', TIMESTAMP WITHOUT TIME ZONE '2017-09-26T11:00:00Z', 2);
 
 INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES (1 , 1);
 INSERT INTO tb_participante_atividade(participante_id, atividade_id) VALUES (1 , 2);
